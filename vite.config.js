65904ae7a642fc4c09ce9331bee0dded1cd0cb7f
Ajay8489 <107861807+Ajay8489@users.noreply.github.com>
2026-09-23 22:13:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  root: process.cwd(),
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'login.html'),
+        main: resolve(__dirname, 'index.html'),
         dashboard: resolve(__dirname, 'dashboard.html'),
       },
     },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
